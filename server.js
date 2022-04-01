ab86b31f5491861bd1ad4e85f01423f719994e2d
Daniel Kuntz <kuntzd99@gmail.com>
2022-03-31 22:45:51 -0500

    Deployed to heroku

--- server.js
+++ server.js
@@ -7,11 +7,16 @@ const PORT = process.env.PORT || 3001
 
 const app = express()
 
+app.use(express.static(`${__dirname}/client/build`))
 app.use(cors())
 app.use(express.json())
 app.use(logger('dev'))
 
 app.use('/api', routes)
+
+app.get('/*', (req, res) => {
+  res.sendFile(`${__dirname}/client/build/index.html`)
+})
 
 app.listen(PORT, () => {
   console.log(`Express server listening on port ${PORT}`)
