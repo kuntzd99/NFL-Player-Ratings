b2c4b70b7d57e57b68d97883bf8f8fdf8dc646ad
Daniel Kuntz <kuntzd99@gmail.com>
2022-04-01 09:40:24 -0500

    Tyring to fix deployment

--- server.js
+++ server.js
@@ -7,12 +7,13 @@ const PORT = process.env.PORT || 3001
 
 const app = express()
 
-app.use(express.static(`${__dirname}/client/build`))
 app.use(cors())
 app.use(express.json())
 app.use(logger('dev'))
 
 app.use('/api', routes)
+
+app.use(express.static(`${__dirname}/client/build`))
 
 app.get('/*', (req, res) => {
   res.sendFile(`${__dirname}/client/build/index.html`)
